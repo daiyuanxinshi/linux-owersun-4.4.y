1370fb00035cfb7e87c27c5805b9b8a36c678e7d
Zhikang Zhang <zhangzhikang1@huawei.com>
2019-01-03 20:06:38 +0800

    f2fs: fix compile warnings: 'struct *' declared inside parameter list
    We meet these compile warnings below, which caused by missing declare structs:
    struct f2fs_io_info, struct extent, struct f2fs_sb_info.
    
    warning: 'struct f2fs_io_info' declared inside parameter list
    warning: 'struct extent_info' declared inside parameter list
    warning: 'struct f2fs_sb_info' declared inside parameter list
    
    Signed-off-by: Zhikang Zhang <zhangzhikang1@huawei.com>
    Reviewed-by: Chao Yu <yuchao0@huawei.com>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    
    rockchip 746a1117c57ad4cdbc2ae38568ad115af8d92336

--- include/trace/events/f2fs.h
+++ include/trace/events/f2fs.h
@@ -116,6 +116,9 @@ TRACE_DEFINE_ENUM(CP_DISCARD);
 		{ CP_RECOVERY,	"Recovery" },				\
 		{ CP_DISCARD,	"Discard" })
 
+struct f2fs_sb_info;
+struct f2fs_io_info;
+struct extent_info;
 struct victim_sel_policy;
 struct f2fs_map_blocks;
 
